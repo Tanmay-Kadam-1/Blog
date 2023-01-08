ae45ab84b4bb8c3bd29f2ce6e3f57e8ac42c1062
Tanmay Kadam <justcoding1482@gmail.com>
2023-01-08 21:59:33 +0530

    Fetched Data and Added Posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { getPosts } from "../services";
 
 import { PostCard, Categories, PostWidget } from "../components";
 
-const posts = [
-	{
-		title: "React Testing",
-		exerpt: "This is an testing Blogg in NextJS",
-	},
-	{
-		title: "React Testing 2",
-		exerpt: "This is an testing Blogg 2 in NextJS",
-	},
-];
-
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }) => {
 	return (
 		<div className="container mx-auto py-10 mb-8">
 			<Head>
@@ -24,7 +14,7 @@ const Home: NextPage = () => {
 			<div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
 				<div className="lg:col-span-8 col-span-1">
 					{posts.map((post) => (
-						<PostCard post={post} key={post.title} />
+						<PostCard post={post.node} key={post.title} />
 					))}
 				</div>
 
@@ -38,5 +28,12 @@ const Home: NextPage = () => {
 		</div>
 	);
 };
+
+export async function getStaticProps() {
+	const posts = (await getPosts()) || [];
+	return {
+		props: { posts },
+	};
+}
 
 export default Home;
